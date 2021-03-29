707d8da0a261966294cf2bb6cde0e833e6997f81
Dan Colmenares <mailonbikedev@gmail.com>
2021-03-29 17:35:13 -0500

    Mejorando un poco las vistas iniciales del Home y Perfil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,30 @@
 <template>
-	<div
-		class="max-w-lg bg-gray-100 shadow-lg rounded-xl overflow-hidden my-40 mx-auto py-4 px-8"
-	>
-		<h2 class="text-center mb-10">Bienvenidos a Iris</h2>
-
-		<h3 class="mb-4">Por favor, inicia sesión aquí:</h3>
-		<router-link
-			to="/login"
-			class="bg-secondary px-4 py-2 text-white my-4 link rounded-lg"
-			custom
-			v-slot="{ navigate }"
+	<div class="w-full flex flex-col justify-center items-center mt-28">
+		<div
+			class="max-w-lg bg-gray-100 shadow-lg rounded-xl mx-auto py-4 px-8 flex flex-col justify-center"
 		>
-			<span @click="navigate" role="link">
-				Iniciar Sesión
-			</span>
-		</router-link>
+			<img
+				id="profile-img"
+				src="@/assets/MoB-logo.png"
+				alt="Mail On Bike Logo"
+				class="w-40 mx-auto relative -top-24"
+			/>
+
+			<h2 class="text-center -mt-16 mb-10 text-2xl text-primary font-bold">
+				Bienvenidos a Mail On Bike
+			</h2>
+
+			<router-link
+				to="/login"
+				class="bg-secondary px-4 py-2 text-white my-4 link rounded-lg text-center font-bold"
+				custom
+				v-slot="{ navigate }"
+			>
+				<span @click="navigate" role="link">
+					Empezar
+				</span>
+			</router-link>
+		</div>
 	</div>
 </template>
 
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,31 +1,41 @@
 <template>
-	<div class="w-96 bg-white p-4 mt-20 mx-auto rounded">
-		<h2>
-			<strong class="resalta">Perfil:</strong>
-			{{ currentUser.username }}
-		</h2>
-		<p>
-			<strong class="resalta">Nombre:</strong>
-			{{ currentUser.fullName }}
-		</p>
-		<p>
-			<strong class="resalta">Id:</strong>
-			{{ currentUser.id }}
-		</p>
-		<p>
-			<strong class="resalta">Email:</strong>
-			{{ currentUser.email }}
-		</p>
-		<p>
-			<strong class="resalta">Empresa:</strong>
-			{{ currentUser.empresa }}
-		</p>
-		<strong class="resalta">Autorizaciones:</strong>
-		<ul>
-			<li v-for="(role, index) in currentUser.roles" :key="index">
-				{{ role }}
-			</li>
-		</ul>
+	<div class="bg-gray-100 shadow-lg rounded-xl w-1/3 mx-auto p-4 my-12">
+		<div class="flex justify-end">
+			<h1
+				class="inline-block text-2xl text-primary text-center font-bold mb-4 rounded-xl relative -top-12 py-2 bg-gray-100 px-6"
+			>
+				Perfil
+			</h1>
+		</div>
+
+		<div class="-mt-12">
+			<h2>
+				<strong class="resalta">Perfil:</strong>
+				{{ currentUser.username }}
+			</h2>
+			<p>
+				<strong class="resalta">Nombre:</strong>
+				{{ currentUser.fullName }}
+			</p>
+			<p>
+				<strong class="resalta">Id:</strong>
+				{{ currentUser.id }}
+			</p>
+			<p>
+				<strong class="resalta">Email:</strong>
+				{{ currentUser.email }}
+			</p>
+			<p>
+				<strong class="resalta">Empresa:</strong>
+				{{ currentUser.empresa }}
+			</p>
+			<strong class="resalta">Autorizaciones:</strong>
+			<ul>
+				<li v-for="(role, index) in currentUser.roles" :key="index">
+					{{ role }}
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
