52b5b8bb38a8e9a1a3d6a82ebf2ac65a61fb1bcf
Dan <mailonbikedev@gmail.com>
2021-06-25 10:41:29 -0500

    Revisando la formula del ruido

--- src/services/ecoamigable.service.js
+++ src/services/ecoamigable.service.js
@@ -2,7 +2,7 @@ export default function calcularEstadisticas(distancia) {
   const emisionAutomovil = 97.8 / 1000; // 100 gramos de CO2 por cada Km
   const emisionBicicleta = 8.6 / 1000; // 8.6 gramos de CO2 por cada Km
 
-  const velocidadPromedioBicicleta = 15; // 15 Km/h velocidad estimada de un ciclista
+  // const velocidadPromedioBicicleta = 15; // 15 Km/h velocidad estimada de un ciclista
   const velocidadPromedioAutomovil = 40; // 40 Km/h velocidad estimada de los vehículos en la ciudad de Lima
 
   const co2 = +(
@@ -10,10 +10,7 @@ export default function calcularEstadisticas(distancia) {
     distancia * emisionBicicleta
   ).toFixed(1);
 
-  const ruido = +(
-    distancia / velocidadPromedioAutomovil -
-    distancia / velocidadPromedioBicicleta
-  ).toFixed(2);
+  const ruido = +(distancia / velocidadPromedioAutomovil).toFixed(2);
 
   return { co2, ruido };
 }
