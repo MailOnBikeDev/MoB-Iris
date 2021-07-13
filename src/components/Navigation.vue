3036d117ea15e6017764552295d4439b6a83ba22
Dan <mailonbikedev@gmail.com>
2021-07-13 09:31:04 -0500

    Font bold en la navegacion

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,164 +1,161 @@
 <template>
-	<nav class="z-40 flex items-center px-6 py-2 text-white bg-secondary">
-		<div>
-			<router-link to="/" custom v-slot="{ navigate }">
-				<img
-					@click="navigate"
-					role="link"
-					class="w-14"
-					src="@/assets/MoB-logo.png"
-					alt="Mail On Bike Logo"
-				/>
-			</router-link>
-		</div>
+  <nav class="z-40 flex items-center px-6 py-2 text-white bg-secondary">
+    <div>
+      <router-link to="/" custom v-slot="{ navigate }">
+        <img
+          @click="navigate"
+          role="link"
+          class="w-14"
+          src="@/assets/MoB-logo.png"
+          alt="Mail On Bike Logo"
+        />
+      </router-link>
+    </div>
 
-		<div v-if="currentUser" class="flex flex-row items-center ml-auto">
-			<!-- Navigation -->
-			<div v-for="nav in navigationMenu" :key="nav.title">
-				<router-link
-					:to="nav.link"
-					custom
-					v-slot="{ navigate }"
-					class="mr-6 link"
-				>
-					<span @click="navigate" role="link">
-						{{ nav.title }}
-					</span>
-				</router-link>
-			</div>
+    <div
+      v-if="currentUser"
+      class="flex flex-row items-center ml-auto font-bold"
+    >
+      <!-- Navigation -->
+      <div v-for="nav in navigationMenu" :key="nav.title">
+        <router-link
+          :to="nav.link"
+          custom
+          v-slot="{ navigate }"
+          class="mr-6 link"
+        >
+          <span @click="navigate" role="link">
+            {{ nav.title }}
+          </span>
+        </router-link>
+      </div>
 
-			<!-- Dropdown Menu -->
-			<div>
-				<button
-					class="px-4 py-2 font-bold text-white bg-info rounded-xl focus:outline-none"
-					@click="dropMenu = !dropMenu"
-				>
-					{{ currentUser.username }}
-				</button>
+      <!-- Dropdown Menu -->
+      <div>
+        <button
+          class="px-4 py-2 font-bold text-white bg-info rounded-xl focus:outline-none"
+          @click="dropMenu = !dropMenu"
+        >
+          {{ currentUser.username }}
+        </button>
 
-				<div
-					v-if="dropMenu"
-					class="absolute z-40 flex flex-col mt-1 text-sm text-black bg-white rounded shadow-xl sub-menu right-4"
-					v-click-outside="clickExterno"
-				>
-					<div
-						v-for="link in linksDropMenu"
-						:key="link.title"
-						class="px-2 py-2 rounded link hover:bg-blue-500 hover:text-white"
-					>
-						<router-link :to="link.link" custom v-slot="{ navigate }">
-							<span @click="navigate" role="link">
-								{{ link.title }}
-							</span>
-						</router-link>
-					</div>
-					<!-- <div
-						class="px-2 py-2 rounded link hover:bg-info hover:text-white"
-						@click="actualizarDatos"
-					>
-						Actualizar datos
-					</div> -->
-					<div class="divider"></div>
-					<a
-						class="px-2 py-2 rounded link hover:bg-blue-500 hover:text-white"
-						href
-						@click.prevent="logOut"
-						>Cerrar Sesión</a
-					>
-				</div>
-			</div>
-		</div>
+        <div
+          v-if="dropMenu"
+          class="absolute z-40 flex flex-col mt-1 text-sm text-black bg-white rounded shadow-xl sub-menu right-4"
+          v-click-outside="clickExterno"
+        >
+          <div
+            v-for="link in linksDropMenu"
+            :key="link.title"
+            class="px-2 py-2 rounded link hover:bg-blue-500 hover:text-white"
+          >
+            <router-link :to="link.link" custom v-slot="{ navigate }">
+              <span @click="navigate" role="link">
+                {{ link.title }}
+              </span>
+            </router-link>
+          </div>
+          <div class="divider"></div>
+          <a
+            class="px-2 py-2 rounded link hover:bg-blue-500 hover:text-white"
+            href
+            @click.prevent="logOut"
+            >Cerrar Sesión</a
+          >
+        </div>
+      </div>
+    </div>
 
-		<div class="content-center ml-auto mr-4" v-else>
-			<div class="flex mr-4">
-				<router-link to="/login" custom v-slot="{ navigate }" class="link">
-					<span @click="navigate" role="link">
-						Login
-					</span>
-				</router-link>
-			</div>
-		</div>
-	</nav>
+    <div class="content-center ml-auto mr-4" v-else>
+      <div class="flex mr-4">
+        <router-link to="/login" custom v-slot="{ navigate }" class="link">
+          <span @click="navigate" role="link">
+            Login
+          </span>
+        </router-link>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 import vClickOutside from "v-click-outside";
 
 export default {
-	data() {
-		return {
-			dropMenu: false,
-			linksDropMenu: [
-				{ title: "Mi Perfil", link: "/perfil" },
-				{ title: "Crear usuario", link: "/register" },
-				{ title: "Gestionar usuarios", link: "/register" },
-				// { title: "Mis Estadísticas", link: "/register" },
-			],
-			navigationMenu: [
-				{
-					title: "Finanzas",
-					link: "/finanzas/historial-pedidos",
-				},
-				{
-					title: "Clientes",
-					link: "/clientes/tablero-clientes",
-				},
-				{
-					title: "MoBikers",
-					link: "/mobikers/equipo-mobiker",
-				},
-				{
-					title: "Pedidos",
-					link: "/pedidos/tablero-pedidos",
-				},
-				// {
-				// 	title: "Testing",
-				// 	link: "/testing",
-				// },
-			],
-		};
-	},
-	computed: {
-		currentUser() {
-			return this.$store.state.user;
-		},
-	},
-	directives: {
-		vClickOutside: vClickOutside.directive,
-	},
-	methods: {
-		logOut() {
-			this.$store.dispatch("logout");
-			this.dropMenu = false;
-			this.$router.push("/login");
-		},
-		clickExterno() {
-			this.dropMenu = false;
-		},
-		// async actualizarDatos() {
-		// 	try {
-		// 		console.log("Actualizando datos");
-		// 		// MoBikers
-		// 		await this.$store.dispatch("mobikers/getMobikers");
-		// 		await this.$store.dispatch("clientes/getClientes");
-		// 		this.dropMenu = false;
-		// 		console.log("Datos perfectamente cargados");
-		// 	} catch (error) {
-		// 		console.error(`Error al cargar los datos. ${error.message}`);
-		// 	}
-		// },
-	},
+  data() {
+    return {
+      dropMenu: false,
+      linksDropMenu: [
+        { title: "Mi Perfil", link: "/perfil" },
+        { title: "Crear usuario", link: "/register" },
+        { title: "Gestionar usuarios", link: "/register" },
+        // { title: "Mis Estadísticas", link: "/register" },
+      ],
+      navigationMenu: [
+        {
+          title: "Finanzas",
+          link: "/finanzas/historial-pedidos",
+        },
+        {
+          title: "Clientes",
+          link: "/clientes/tablero-clientes",
+        },
+        {
+          title: "MoBikers",
+          link: "/mobikers/equipo-mobiker",
+        },
+        {
+          title: "Pedidos",
+          link: "/pedidos/tablero-pedidos",
+        },
+        // {
+        // 	title: "Testing",
+        // 	link: "/testing",
+        // },
+      ],
+    };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.user;
+    },
+  },
+  directives: {
+    vClickOutside: vClickOutside.directive,
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch("logout");
+      this.dropMenu = false;
+      this.$router.push("/login");
+    },
+    clickExterno() {
+      this.dropMenu = false;
+    },
+    // async actualizarDatos() {
+    // 	try {
+    // 		console.log("Actualizando datos");
+    // 		// MoBikers
+    // 		await this.$store.dispatch("mobikers/getMobikers");
+    // 		await this.$store.dispatch("clientes/getClientes");
+    // 		this.dropMenu = false;
+    // 		console.log("Datos perfectamente cargados");
+    // 	} catch (error) {
+    // 		console.error(`Error al cargar los datos. ${error.message}`);
+    // 	}
+    // },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .divider {
-	width: 100%;
-	height: 2px;
-	background: #303868;
+  width: 100%;
+  height: 2px;
+  background: #303868;
 }
 
 .link {
-	cursor: pointer;
+  cursor: pointer;
 }
 </style>
