faeb90e908f58fceb7d2d39b47928c926054260b
Javier Torres <javiertorresm2000@gmail.com>
2021-07-15 13:08:59 -0500

    Cambios para poder Editar Ruteo

--- src/components/ResumenRuteo.vue
+++ src/components/ResumenRuteo.vue
@@ -25,16 +25,16 @@
     <InfoRuteo :infoRuta="currentRuta" />
 
     <div class="flex flex-row justify-around mt-6">
-      <!-- <router-link
-      :to="`/pedidos/tablero-pedidos/${currentPedido.id}`"
+      <router-link
+      :to="`/pedidos/ruteo/${currentRuta.ruta.id}`"
         custom
         v-slot="{ navigate }"
-        class="px-6 py-2 font-bold text-white bg-green-600 rounded-xl focus:outline-none hover:bg-green-500"
+        class="px-6 py-2 font-bold text-white bg-yellow-600 rounded-xl focus:outline-none hover:bg-yellow-500"
       >
         <span @click="navigate" role="link" class="text-center cursor-pointer"
           >Editar Ruteo</span
         >
-      </router-link> -->
+      </router-link>
 
       <button
         @click.prevent="emitirComandaRuteo"
